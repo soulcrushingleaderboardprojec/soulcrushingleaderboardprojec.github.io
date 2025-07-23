20b612079be1eae6f726b1eac691a22d35e2c815
soulcrushingleaderboardprojec <henriquepassis616@gmail.com>
2025-07-23 12:51:57 +0200

    Create pagehandler.js

---
+++ pagehandler.js
@@ -0,0 +1,23 @@
+function g(element_id) {
+  return document.getElementById(element_id);
+}
+g("tower-lookup-page").style.display = "none";
+g("leaderboard-page").style.display = "none";
+function open_page(page_num) {
+  // 1 - Home
+  // 2 - Towers
+  // 3 - Leaderboard
+  // 4 - Games
+  g("menu-page").style.display = "none";
+  g("tower-lookup-page").style.display = "none";
+  g("leaderboard-page").style.display = "none";
+  if (page_num == 1) {
+    g("menu-page").style.display = "";
+  } else if (page_num == 2) {
+    g("tower-lookup-page").style.display = "";
+  } else if (page_num == 3) {
+    g("leaderboard-page").style.display = "";
+  } else if (page_num == 4) {
+    g("game-links-page").style.display = "";
+  }
+}
