4605d3f6bf9a4b805849121f288ecb5ffc1b8b9f
soulcrushingleaderboardprojec <henriquepassis616@gmail.com>
2025-08-01 16:10:27 +0200

    Update towermanager.js

--- static/towermanager.js
+++ static/towermanager.js
@@ -441,3 +441,8 @@ if (params.get("u")) {
     open_page("Leaderboard");
     open_player(params.get("u"));
 }
+
+let old_url = "soulcrushingleaderboardprojec.github.io";
+if (window.location.hostname == old_url) {
+    window.location.href = window.location.href.replace(old_url, "soulcrushingleaderboardprojec.vercel.app");
+}
